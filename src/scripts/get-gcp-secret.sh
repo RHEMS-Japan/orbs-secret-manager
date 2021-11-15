0fbc1a9db1051aa710fe14b60a0b01edc7bf4151
shindex-rhems <shindex@rhems-japan.co.jp>
2021-11-15 12:26:44 +0900

    list

--- src/scripts/get-gcp-secret.sh
+++ src/scripts/get-gcp-secret.sh
@@ -1,9 +1,9 @@
 _secret_name=$(eval echo "$SECRET_NAME")
 
-_latest_ver=$(gcloud secrets versions list ${_secret_name} --limit=1 | awk '{print $1}' | sed -n 2p)
-echo LATEST_VER = ${_latest_ver}
+_latest_ver=$(gcloud secrets versions list "${_secret_name}" --limit=1 | awk '{print $1}' | sed -n 2p)
+echo LATEST_VER = "${_latest_ver}"
 
-gcloud secrets versions access ${_latest_ver} \
-  --secret=${_secret_name} --format='get(payload.data)' |
+gcloud secrets versions access "${_latest_ver}" \
+  --secret="${_secret_name}" --format='get(payload.data)' |
   tr '_-' '/+' |
-  base64 -d >>./${OUTPUT_PATH}
+  base64 -d >> "./${OUTPUT_PATH}"
